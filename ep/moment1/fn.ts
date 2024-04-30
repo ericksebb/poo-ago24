79be6be94b18b6f818aea00db3fb373d3525fc06
Erick Sebastián <esprado@sudamericano.edu.ec>
2024-04-30 09:30:38 -0500

    Spaceship beta

--- ep/moment1/fn.ts
+++
@@ -1,9 +0,0 @@
-import { Event, Planet } from "./exports";
-
-export function explore(direction: "up" | "down" | "left" | "right"): void {}
-  
-export function collectResources<T>(planet: Planet, quantity: number): T[] {
- return [];
-}
-  
-export function eventHandler(event: Event): void {}
--- ep/moment1/run.ts
+++ ep/moment1/run.ts
@@ -1,8 +1,9 @@
-import { Planet, Resource, Spaceship, Event } from "./exports";
-import * as fn from "./fn";
-const readline = require('readline');
+import { Planet, Resource } from "./src/planet_constructor";
+import { Event } from "./src/event_interface";
+import { Spaceship } from "./src/spaceship_interface";
+import * as fn from "./tmp/fn"
 
-// Lógica del simulador
+// Let's create our environment
 const ship: Spaceship = { codeName: "CBRPNK-2077", hp: 3000, speed: Infinity, cargo: 100};
 const planet1 = new Planet("Europa", 30000, "Grineer", Resource.Polymer, false, true);
 const event1: Event = { name: "Landing in Europa", date: new Date(), isHit: false, isCargoLoaded: true};
@@ -10,30 +11,9 @@ const event1: Event = { name: "Landing in Europa", date: new Date(), isHit: fals
 fn.explore("left");
 fn.collectResources<number>(planet1, 10);
 fn.eventHandler(event1);
-
 console.log(`Welcome abroad, captain. Your ship ${ship.codeName} currently has ${ship.hp} HP and ${ship.cargo} cargo capacity, We're good to go!.
 You are currently in ${planet1.name} and the event ${event1.name} is happening right now.
 
 `);
 console.log(`Here is a brief on ${planet1.name}: It has a population of ${planet1.population} and is affiliated with the ${planet1.afiliation}
 It's common resource is ${planet1.commonResource} and it is ${planet1.isHostile ? "hostile" : "friendly"} and as of today, ${planet1.name} is ${planet1.isColonized ? "colonized" : "not colonized"}.`);
-function promptCollectResources() {
- const rl = readline.createInterface({
- input: process.stdin,
- output: process.stdout
- });
-
- rl.question(`Do you want to collect ${planet1.commonResource}? (Y/N): `, (answer) => {
- if (answer.toUpperCase() === 'Y') {
-  // Call the collectResources function here
-  fn.collectResources<number>(planet1, 10);
- } else if (answer.toUpperCase() === 'N') {
-  console.log('Resource collection skipped.');
- } else {
-  console.log('Invalid input. Please enter Y or N.');
- }
- rl.close();
- });
-}
-
-promptCollectResources();
