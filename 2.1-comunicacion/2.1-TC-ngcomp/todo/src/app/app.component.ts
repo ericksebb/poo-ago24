033e0f08be8db391f48aaa927cdc7a63831f3e0e
E. Sebastián Prado <esprado@sudamericano.edu.ec>
2024-05-26 23:29:28 -0500

    add todo angular project

---
+++ 2.1-comunicacion/2.1-TC-ngcomp/todo/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { PageComponent } from './pages/page/page.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, PageComponent, WelcomeComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isCollapsed = false;
+}
